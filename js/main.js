677bc12703833f0a5408bd35cea765c28aeb5885
Nelasov92 <kapam6a2101@gmail.com>
2019-02-13 17:07:19 +0200

    13.02.19

---
+++ js/main.js
@@ -0,0 +1,14 @@
+'use strict';
+
+
+(function($){
+	$(document).ready(function() {
+
+		//slider
+		$('.slider').slick({
+			dots: true,
+			arrows: false,
+			infinite: true
+		});
+	});
+})(jQuery);
